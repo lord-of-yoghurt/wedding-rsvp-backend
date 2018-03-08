8f16e5bc63bf2aee9e55452ea28b02f88d422c19
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-08 08:56:35 -0500

    set up POST /responses endpoint

--- server.js
+++ server.js
@@ -10,13 +10,25 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// *************************** //
+// ******************************************* //
 
 app.post('/responses', (req, res) => {
+    const {
+        firstName, lastName, email, allergies, additionalGuests,
+        attending, mealPreference, notes
+    } = req.body;
 
+    const newResp = new RSVPResponse({
+        firstName, lastName, email, allergies, additionalGuests,
+        attending, mealPreference, notes
+    });
+
+    newResp.save()
+        .then((doc) => res.send({ status: 'Peachy!' }))
+        .catch((e) => res.status(400).send({ status: 'Not peachy.' }));
 });
 
-
+// ******************************************* //
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
