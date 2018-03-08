47f85e1d4a018bb2ddd05b88b7066c4594bf7ecd
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-07 20:21:36 -0500

    add server settings

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const express = require('express'),
 
 const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
