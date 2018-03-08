57ad62f057926decc4068df85d6d467e0add456f
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-08 09:35:50 -0500

    make settings for heroku deploy

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(bodyParser.json());
 
 // ******************************************* //
 
+app.get('/', (req, res) => {
+    res.send('Sup!');
+});
+
 app.post('/responses', (req, res) => {
     const {
         firstName, lastName, email, allergies, additionalGuests,
